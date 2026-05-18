1ef8acad8f3a02d0549fb431c4b6ba766c34046d
Andre Tavares <andretavares16@gmail.com>
2026-05-18 16:27:20 -0300

    diminuindo o tamanho dos cards dos produtos na tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,7 +143,7 @@ export default async function Home({
               </p>
             </div>
           ) : (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2">
               {filtered.map((product: Product) => (
                 <ProductCard
                   key={product.id}
